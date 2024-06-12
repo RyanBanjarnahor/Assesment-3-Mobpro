511693e03747bec71437142b1ab8e81ba86c2543
Ryan Gusman Yoel Banjarnahor <ryanbanjarnahor3@gmail.com>
2024-06-13 03:23:35 +0700

    Menambahkan package model file nya

---
+++ app/src/main/java/org/d3if0098/assesment3mobpro/model/Hewan.kt
@@ -0,0 +1,7 @@
+package org.d3if0098.assesment3mobpro.model
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String,
+    val imageId: String
+)
---
+++ app/src/main/java/org/d3if0098/assesment3mobpro/model/User.kt
@@ -0,0 +1,7 @@
+package org.d3if0098.assesment3mobpro.model
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+)
